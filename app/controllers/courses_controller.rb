e1fd5d7a6a804a6a9b7dcd76c6a31e6da1a7c151
Arttu Tervo <arttu.tervo@gmail.com>
2009-11-12 00:50:34 +0200

    show action for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,8 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
+  
+  def show
+    @course = Course.first(params[:id])
+  end
 end
--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe CoursesController do
   integrate_views
-  
+
   it "should show index action" do
     get "index"
     
@@ -25,4 +25,13 @@ describe CoursesController do
     response.should have_tag("td.description", "desc3")
   end
 
+  it "should one course" do
+    course = Course.create(:name => "sample course", :description => "very short description")
+    
+    get "show", :id => course.id
+    
+    response.should be_success
+    response.should have_tag('h3.course_name', 'sample course')
+    response.should have_tag('p.description', 'very short description')
+  end
 end
