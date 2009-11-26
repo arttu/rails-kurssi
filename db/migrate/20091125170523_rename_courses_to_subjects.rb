8b3fec6da9c04d3e479ddaa51f52105fc49e2a9c
Arttu Tervo <arttu.tervo@gmail.com>
2009-11-27 01:43:46 +0200

    removed obsolete migration

--- db/migrate/20091125170523_rename_courses_to_subjects.rb
+++
@@ -1,9 +0,0 @@
-class RenameCoursesToSubjects < ActiveRecord::Migration
-  def self.up
-    rename_table :courses, :subjects
-  end
-
-  def self.down
-    rename_table :subjects, :courses
-  end
-end
