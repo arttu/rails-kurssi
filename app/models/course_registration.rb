0d2ad6992758c717b5d026a055701408debcaefd
Arttu Tervo <arttu.tervo@gmail.com>
2009-12-11 02:44:43 +0200

    google chart pie for top courses

--- app/models/course_registration.rb
+++ app/models/course_registration.rb
@@ -3,4 +3,13 @@ class CourseRegistration < ActiveRecord::Base
   belongs_to :exercise_group
   belongs_to :user
   has_many :students, :source => :user
+  
+  def self.top_5_attendeed_courses
+    counts = count(:group => "exercise_group_id", :order => "count_all desc", :limit => 5, :include => :exercise_group)
+    arr = []
+    counts.each_pair { |eg_id, count|
+      arr << [ExerciseGroup.find(eg_id).course, count]
+    }
+    arr
+  end
 end
